dea88dea43442475b4bba29e35fa0ab85e367b30
fillip husebø <felipehusebo@gmail.com>
2025-04-06 22:41:07 +0200

    set up project structure

---
+++ src/js/router/index.js
@@ -0,0 +1,25 @@
+export default async function router(pathname = window.location.pathname) {
+    switch (pathname) {
+        case "/":
+            await import("./views/home.js");
+            break;
+        case "/auth/login/":
+            await import("./views/login.js");
+            break;
+        case "/auth/register/":
+            await import("./views/register.js");
+            break;
+        case "/listing/":
+            await import("./views/listing.js");
+            break;
+        case "/listing/create/":
+            await import("./views/listingCreate.js");
+            break;
+        case "/listing/edit/":
+            await import("./views/listingEdit.js");
+            break;
+
+        default:
+            await import("./views/notFound.js");
+    }
+}
