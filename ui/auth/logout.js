343adf1ddda8e7638ffd5cf63b415b01e46de6cc
fillip husebø <felipehusebo@gmail.com>
2025-04-20 14:24:43 +0200

    added pagination to home page

--- ui/auth/logout.js
+++
@@ -1,7 +0,0 @@
-export function onLogout() {
-    const logoutBtn = document.getElementById("logout-btn");
-    logoutBtn.addEventListener("click", ()=> {
-        localStorage.clear();
-        window.location.href = "";
-    })
-}
