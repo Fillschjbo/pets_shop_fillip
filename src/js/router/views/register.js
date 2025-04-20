c2b64dc22c6abd97bb82621c21ad02ed78ddaa7f
fillip husebø <felipehusebo@gmail.com>
2025-04-20 15:55:16 +0200

    made header responsive

--- src/js/router/views/register.js
+++ src/js/router/views/register.js
@@ -1,5 +1,7 @@
 import {onRegister} from "../../../../ui/auth/register.js";
+import {header} from "../../../../ui/global/header.js";
 
 const form = document.forms.register;
 
 form.addEventListener("submit", onRegister);
+header()
